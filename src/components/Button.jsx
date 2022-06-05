3842b073df3487273fd5528bbd3375c22d60cbf0
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-06-05 23:30:07 +0530

    base styles, navbar, assessts

---
+++ src/components/Button.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Button({ text, icon = false, subduedButton = false }) {
+	return (
+		<Div className="flex j-center a-center">
+			{icon ? (
+				<button
+					className={` flex j-center a-center gap-1 ${
+						subduedButton ? 'subduedButton' : ''
+					}`}
+				>
+					{text} {icon}
+				</button>
+			) : (
+				<button className={`${subduedButton ? 'subduedButton' : ''}`}>
+					{text}
+				</button>
+			)}
+		</Div>
+	);
+}
+const Div = styled.div`
+	button {
+		padding: 0.7rem 1.3rem;
+		background-color: var(--blue);
+		color: var(--primary);
+		border: none;
+		border-radius: 0.5rem;
+		font-size: 1.1rem;
+		cursor: pointer;
+	}
+	.subduedButton {
+		background-color: var(--subdue-button);
+	}
+`;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from './Button';
+export default function Navbar() {
+	const links = ['Home', 'Dashboard', 'Learn', 'FAQ', 'Contact', 'About'];
+	return (
+		<Nav className="flex j-between a-center">
+			<div className="brand">
+				<h2>Coinbase</h2>
+			</div>
+			<div className="toggle"></div>
+			<div className="links">
+				<ul className="flex gap-2">
+					{links.map((link) => {
+						return (
+							<li key={link}>
+								<a href="/">{link}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="auth flex gap-1">
+				<Button subduedButton text="Sign In" />
+				<Button text="Sign Up" />
+			</div>
+		</Nav>
+	);
+}
+const Nav = styled.nav`
+	margin: 3rem 5rem;
+	.brand {
+		h2 {
+			font-size: 2.5rem;
+		}
+	}
+	.toggle {
+		display: none;
+	}
+	.links {
+		ul {
+			font-weight: bolder;
+			li {
+				cursor:pointer &:first-of-type {
+					a {
+						opacity: 1;
+					}
+				}
+				a {
+					opacity: 0.7;
+					transition: 0.5s ease-in-out;
+					&:hover {
+						opacity: inital;
+					}
+				}
+			}
+		}
+	}
+`;
